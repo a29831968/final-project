1376aab0802159059ae89edc7a2481d66322b0c1
a29831968 <a29831968@gmail.com>
2018-04-29 01:57:48 +0800

    image size method confirmed

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-$("div").on("click",function(){
-  $("div").animate({height:"100%",width:"100%","borderRadius":"0"});
-  $("img").animate({"height":'auto','max-width':'100%','margin-left':"0",'margin-top':'0'});
+$("#page1").on("click",function(){
+  $("#page1").animate({height:"1200px",width:"1600px","borderRadius":"0"});
+
+  $("img").animate({"height":'100%',"width":"auto",'max-width':'100%','margin-left':"0",'margin-top':'0'});
 })
