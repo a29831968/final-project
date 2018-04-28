669e612b3b531a3878b141844f1193ec66e7ffdc
a29831968 <a29831968@gmail.com>
2018-04-29 00:39:27 +0800

    test file upload

---
+++ test.js
@@ -0,0 +1,4 @@
+$("div").on("click",function(){
+  $("div").animate({height:"100%",width:"100%","borderRadius":"0"});
+  $("img").animate({"height":'auto','max-width':'100%','margin-left':"0",'margin-top':'0'});
+})
