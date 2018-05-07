8db63acde9e37b23ec0cc0d0d1ccf2fbc0049e34
a29831968 <a29831968@gmail.com>
2018-05-07 21:18:44 +0800

    build server(express), and set gitignore.

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = 10069
+
+app.listen(port)
+app.use(express.static(__dirname + '/public'))
+app.use(express.json()); // to support JSON-encoded bodies
+app.use(express.urlencoded({extended: true})); // to support URL-encoded bodies
+
+// get json file from the file: name.json
+var fs = require('fs');
+var obj;
+fs.readFile('./public/name.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    obj = JSON.parse(data);
+});
+
+app.post('/get_data', function(req, res){
+  // req.body.idname, is how you get from html
+  var idname = req.body.idname;
+  var name=obj[idname];
+  res.send(`<h1>Student's name is: ${name}.</h1>`)
+})
+
+app.get("/ajax_data", function(req, res) {
+  // req.query.idname, can retrieve value of idname from the .js file
+  var id=req.query.idname;
+  var name=obj[id];
+  res.send(`Student's name is ${name}.`)
+})
