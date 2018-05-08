82edacd6a9a206ebeab175c3d365b2c13e483c6e
st2k8 <35991525+st2k8@users.noreply.github.com>
2018-05-09 00:43:19 +0900

    updated login page

--- login/login.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $("#frame").animate({"height":"12vw", "width":"36vw"
-  ,"margin-left":"-18vw", "margin-top":"-6vw"
-  ,"left":"50vw", "top":"60vw"},1800);
-  }    
-)
