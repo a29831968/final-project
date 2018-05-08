4cb004da8cbd05f949be3fba9ed53ce7606dbfcc
a29831968 <a29831968@gmail.com>
2018-05-08 22:02:13 +0800

    login, modified

--- login/login.js
+++ login/login.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-  $("#frame").animate({"height":"6vh", "width":"28vw"
-  ,"margin-left":"-14vw", "margin-top":"-3vh"
-  ,"left":"50vw", "top":"50vh"},1800);
+  $("#frame").animate({"height":"12vw", "width":"36vw"
+  ,"margin-left":"-18vw", "margin-top":"-6vw"
+  ,"left":"50vw", "top":"60vw"},1800);
   }    
 )
