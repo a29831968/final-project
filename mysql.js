1bdb66d76df02ba44d8c1b2dae6c5c164dbf47f1
a29831968 <a29831968@gmail.com>
2018-05-15 23:08:34 +0800

    using tmux to let server run.

--- mysql.js
+++ mysql.js
@@ -5,9 +5,21 @@ var con = mysql.createConnection({
   host:"localhost",
   user:"uidd2018_groupG",
   password:"webGGdata",
+  // Our database-name: uidd2018_groupG
+  database: "uidd2018_groupG"
 });
 
 con.connect(function(err){
   if(err) throw err;
   console.log("connected!");
+  // create table {id, name, level, exp}
+  //var sql= "CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), lv INT, exp INT)";  
+  var sql = "INSERT INTO users (name, lv, exp) VALUES ('John', 2, 220)";
+  con.query(sql, function(err, result){
+    if(err) throw err;
+    var row=result;
+    //console.log(row[name]);
+  })
 });
+
+
