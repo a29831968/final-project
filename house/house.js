d5dc02d10aede85268614bde612ca2f71302f57a
eedie9712 <qw1562435@gmail.com>
2018-05-23 00:10:09 +0800

    letterpart

--- house/house.js
+++ house/house.js
@@ -1,9 +1,12 @@
+$(document).ready(function()
+    {
+       $('.entrance').click(function()
+          {
+            window.location.assign("./letter/letter1.html");            
 
 
 
+          });
 
 
-
-
-
-
+    });
