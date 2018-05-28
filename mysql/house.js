953509ec8d3763b0ad3ffc8bf0f4e31d0cd76f02
a29831968 <a29831968@gmail.com>
2018-05-28 19:09:55 +0800

    finish backend of house buildings function with bugs.

--- mysql/house.js
+++ mysql/house.js
@@ -1,21 +1,65 @@
+// needed info
+var buildings_array=[];
+var objects_array=[];
 $(document).ready(function(){
   console.log("get ready");
+  console.log("get buildings");
   // .ajax get building condition
+  // and set the buildings in every block
   $.ajax({
     method:"get",
     url: "./buildings",
     data:{
     },
     success: function(data){
-      console.log("buidling condition: "+data);
+      buildings_array=data;
+      setBuildings(buildings_array);
+      /*
+      for(var i=0; i<data.length; i++){
+        if(data[i]!=null){
+          console.log((i+1)+": "+data[i]);
+          buildings= "buildings/"+data[i]+".png";
+          img="<img width='100%' src='"+buildings+"'/>";
+          $("#block"+(i+1)).html(img);
+          $("#block"+(i+1)).css("background-color","rgb(255, 255, 255, 0)");
+        }
+        $('#block' +(i+1)).click( createCallback( (i+1), data[i] ) );
+      }
+      */
     }
   })
-
-  for(var i = 1; i <= 20; i++) {
-    $('#block' + i).click( createCallback( i ) );
-  }
+  // .ajax get objects
+  // and set all the objects into the bag
+  console.log("get objects");
+  $.ajax({
+    method: "get",
+    url: "./objects",
+    data:{
+    },
+    success: function(data){
+      console.log("final: "+data);
+      objects_array=data;
+      setobjects(objects_array);
+      /*
+      for(var i=0; i<data.length; i++){
+        var object= "objects/"+data[i]+".png";
+        console.log(object);
+        img="<img width='100%' src='"+object+"'/>";
+        $("#"+i).html(img);
+        console.log(data[i]);
+      }
+      */
+    }
+  })
   $("#testbtn").click(function(){
     $("#flip").css({display:"block"});
+      for(var x=0; x<15; x++){
+        $("#"+x).click(showInfo(x));
+      }
+  })
+  //flip close
+  $("#close").click(function(){
+    $("#flip").css({display:"none"});
   })
 
   $("#flip").turn({
@@ -23,10 +67,115 @@ $(document).ready(function(){
     height:"132vw"
   });
 });
-
-function createCallback( i ){
-  
-  return function(){
-      alert('you clicked' + i);
+// function set building conditions
+function setBuildings(build){
+    for(var i=0; i<build.length; i++){
+      if(build[i]!=null){
+        console.log((i+1)+": "+build[i]);
+        buildings= "buildings/"+build[i]+".png";
+        img="<img width='100%' src='"+buildings+"'/>";
+        $("#block"+(i+1)).html(img);
+        $("#block"+(i+1)).css("background-color","rgb(255, 255, 255, 0)");
+      }
+      $('#block' +(i+1)).click( createCallback( (i+1), build[i] ) );
     }
 }
+// function set objects
+function setobjects(obj){
+    if(obj.length==0){
+      var object= "asset/block.png";
+      console.log(object);
+      img="<img width='100%' src='"+object+"'/>";
+      $("#0").html(img);
+      console.log(obj[i]);
+    }
+    for(var i=0; i<obj.length; i++){
+      if(obj[i]!=null){
+        var object= "objects/"+obj[i]+".png";
+        console.log(object);
+        img="<img width='100%' src='"+object+"'/>";
+        $("#"+i).html(img);
+        console.log(obj[i]);
+      }else{
+        var object= "asset/block.png";
+        console.log(object);
+        img="<img width='100%' src='"+object+"'/>";
+        $("#"+i).html(img);
+        console.log(obj[i]);
+      }
+    }
+}
+// show information
+function showInfo(x){
+  return function(){
+    if(objects_array[x]!=null){
+      console.log("x:"+x);
+      infopic= "info/info"+objects_array[x]+".png";
+      img="<img width='100%' src='"+infopic+"'/>";
+      console.log(img);
+      $(".information").html(img);
+      $(".information").css({display:"block"});
+      $("#mask").css({display:"block"});
+      $("#"+x).unbind();
+    }
+  }
+}
+function createCallback( i , pic){
+  return function(){
+    console.log(i-1);
+    console.log("buildings:"+ pic);
+      if(pic==null){
+        $("#flip").css({display:"block"});
+        for(var x=0; x<15; x++){
+          $("#"+x).click(construct(x,i));
+        }
+      }else{
+        buildingShowInfo(pic);
+      }
+    }
+}
+function buildingShowInfo(pic){
+    infopic= "info/info"+pic+".png";
+    img="<img width='100%' src='"+infopic+"'/>";
+    $(".information").html(img);
+    $(".information").css({display:"block"});
+    $("#mask").css({display:"block"});
+}
+function construct(x, i){
+  return function(){
+    if(objects_array[x]!=null){
+      console.log("x:"+x+" i:"+i);
+      buildings= "buildings/"+objects_array[x]+".png";
+      img="<img width='100%' src='"+buildings+"'/>";
+      $("#block"+(i)).html(img);
+      $("#block"+(i)).css("background-color","rgb(255, 255, 255, 0)");
+      $("#flip").css({display:"none"});
+      $("#"+x).unbind();
+      // change the data in database
+      $.ajax({
+        method:'get',
+        url:'./objANDbuild',
+        data:{
+          x:x,
+          i:i,
+        },
+        success: function(data){
+          console.log("buildings:"+data.buildings_info);
+          console.log("objs:"+data.objs_info);
+          buildings_array=data.buildings_info;
+          setBuildings(buildings_array);
+          objects_array=data.objs_info;
+          setobjects(objects_array);
+        }
+      })
+    }
+  }
+}
+
+$("#mask").click(function(){
+    if($(".information").css('display') == 'block'){
+      $(".information").css({display:"none"});
+      $("#mask").css({display:"none"});
+    }
+})
+
--- mysql/server_v1.js
+++ mysql/server_v1.js
@@ -38,26 +38,105 @@ con.connect(function(err){
 });
 
 
-// retrieve data from table: buildings
 var buildings_info=[];
+var objs_info=[];
 con.connect(function(err){
+  // retrieve data from table: buildings
   con.query("SELECT * FROM buildings WHERE name = ?",user_name, function(err, result){
     if(err) throw err;
-    console.log(result);
-
-    var number=result[0].number;
-    console.log("how many buildings:"+number);
-
     var building=result[0];
     for(var i=1; i<=25;i++){
-      console.log("i: "+i);
-      console.log("result: "+result[0][i]);
       buildings_info.push(result[0][i]);
     }
-    console.log("final: "+buildings_info);
+  })
+  // retreive data from table: objects
+  con.query("SELECT * FROM objects WHERE name = ?",user_name, function(err, result){
+    if(err) throw err;
+    var number=result[0].amount;
+    for(var i=1; i<=25;i++){
+      if(result[0][i]!=null){
+        objs_info.push(result[0][i]);
+      }
+    }
   })
 })
 app.get("/buildings", function(req, res) {
   console.log("send:"+buildings_info);
   res.send(buildings_info);
 })
+app.get("/objects", function(req, res) {
+    console.log(objs_info);
+    res.send(objs_info);
+})
+
+// update data when put buildings into house
+app.get("/objANDbuild", function(req, res) {
+    updateBuildingsAndObjects(req, res);
+    //console.log("back from function");
+    //console.log("changed buildings_info:"+buildings_info);
+    //console.log("changed obj_info:"+objs_info);
+    //res.send();
+})
+
+function updateBuildingsAndObjects(req, res){
+  // step1. put buildings[i]=objects[x]
+  //var sql = "UPDATE buildings SET ? = ? WHERE name = ?",[i, o, user_name];
+  console.log("function: updataBuildingsAndObjects");
+  var x=req.query.x;
+  var o=objs_info[x];
+  console.log("req.query.x: "+x);
+  console.log("o:"+o);
+  var i=req.query.i;
+  var j=buildings_info[i];
+  console.log("req.query.i: "+i);
+  console.log("j:"+j);
+  con.query( "UPDATE buildings SET ?? =? WHERE name = ?",[i, o, user_name], function(err, result){
+    if(err) throw err;
+    console.log("first con.query");
+    // retrieve data from table: buildings
+    buildings_info=[];
+    console.log("empty bu_info:"+ buildings_info);
+    con.query("SELECT * FROM buildings WHERE name = ?",user_name, function(err, result){
+      if(err) throw err;
+      console.log("step1");
+      var building=result[0];
+      for(var i=1; i<=25;i++){
+        buildings_info.push(result[0][i]);
+      }
+      // step2. objects[x] = null
+      con.query("SELECT * FROM objects WHERE name = ?",user_name, function(err, result){
+        if(err) throw err;
+        console.log("step2");
+        var number=result[0].amount;
+        var temp=0;
+        for(var g=1; g<=25;g++){
+          if(result[0][g]!=null){
+            temp=temp+1;
+            if(temp==(x+1)){
+              con.query("UPDATE objects SET ?? = ? WHERE name = ?",[temp, null, user_name], function(error, result){
+                if (err) throw err;
+              });
+            }
+          }
+        }
+        // step3. objects['amount']=objects.amount-1
+        con.query("UPDATE objects SET amount = ? WHERE name = ?",[(number-1), user_name], function(error, result){
+          console.log("step3");
+          if (err) throw err;
+        });
+      });
+      objs_info=[];
+      con.query("SELECT * FROM objects WHERE name = ?",user_name, function(err, result){
+        if(err) throw err;
+        var number=result[0].amount;
+        for(var i=1; i<=number;i++){
+          console.log("last info");
+          objs_info.push(result[0][i]);
+        }
+      })
+    })
+  });
+  console.log("changed buildings_info:"+buildings_info);
+  console.log("changed obj_info:"+objs_info);
+  res.send({buildings_info:buildings_info, objs_info:objs_info});
+}
