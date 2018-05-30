72a215a47d75637465b566809dc6cb1066d01ce7
a29831968 <a29831968@gmail.com>
2018-05-30 09:26:41 +0800

    connect with house and map.

--- mysql/house.js
+++ mysql/house.js
@@ -2,7 +2,36 @@
 var buildings_array=[];
 var objects_array=[];
 $(document).ready(function(){
-  
+  //
+  $("#map").click(function(){
+    window.location.assign("./map.html");
+  })
+  //
+  $('.entrance').click(function(){
+    $('.switchscreen').animate(
+        {
+              'left':'0px',
+              'top':'0px',
+              'width':'100%',
+              'height':'100%'
+        }, 100,
+        function(){
+          window.location.assign("./letter/letter1.html");
+        });
+  });
+
+  // switch profile 
+  $('.small_person').click(function(){
+    $('.change_to_profile').animate(
+        {
+              'left':'0px',
+              'top':'0px',
+              'width':'100vw',
+              'height':'100vh'
+        }, 100,
+        );
+  });
+  //
   console.log("get ready");
   console.log("get buildings");
   // .ajax get building condition
