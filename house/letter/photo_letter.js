48cd52ad29caa4db40cad36ad8da0f18c0f56c8c
eedie9712 <qw1562435@gmail.com>
2018-06-17 21:16:26 +0800

    delete

--- house/letter/photo_letter.js
+++ house/letter/photo_letter.js
@@ -1,8 +1,7 @@
-
-var on=0;
+ 
 $(document).ready(function()
 {
-
+ var on=1;
   $('.next').click(function()     //to the next page
       {
         window.location.assign('../letter/output.html');
@@ -12,24 +11,33 @@ $(document).ready(function()
         window.location.assign('../letter/letter1.html')
  
      });
-   $(".star1").click(
-  function(){
-     $('.star1').attr("src","./picture1/star_yellow");
-
-    });
+   $('.star1').click(function()
+  {  
+    if((on%2)==1)
+    {
+      $('.star1').attr("src","./picture1/star_yellow.png");
+      on++;
+    }
+    else if((on%2)==0)
+    {
+ $('.star1').attr("src","./picture1/star_gray.png");
+      on++;
+    
+    } 
+  });
  $('.star2').click(function()    //to light the star
   {
-      $('.star2').attr("src","./picture1/star_yellow");
+      $('.star2').attr("src","./picture1/star_yellow.png");
       
    });
  $('.star3').click(function()    //to light the star
   {
-      $('.star3').attr("src","./picture1/star_yellow");
+      $('.star3').attr("src","./picture1/star_yellow.png");
       
    });
  $('.star4').click(function()    //to light the star
   {
-      $('.star4').attr("src","./picture1/star_yellow");
+      $('.star4').attr("src","./picture1/star_yellow.png");
       
    });
  $('.star5').click(function()    //to light the star
