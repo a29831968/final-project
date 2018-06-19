0fc9afe1b331b7237dc17bdc6d0122a04303eb1e
eedie9712 <qw1562435@gmail.com>
2018-06-19 17:32:58 +0800

    new

---
+++ eddie/public/js/addnewletter.js
@@ -0,0 +1,12 @@
+$(document).ready(function()
+{   
+ $('.close').click(function()   //back to last page
+  {
+      window.location.assign('../letter/letter1.html') 
+  });
+ $('.next').click(function()   //back to the finish page
+     {  
+       
+        window.location.assign('../letter/letter1.html')
+     });
+});
