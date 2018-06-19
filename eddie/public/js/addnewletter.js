2c9ee781f8f29d36172e5a31fae8007801da54b6
eedie9712 <qw1562435@gmail.com>
2018-06-20 00:54:10 +0800

    new

--- eddie/public/js/addnewletter.js
+++ eddie/public/js/addnewletter.js
@@ -1,3 +1,4 @@
+var statusofupload=0;
 $(document).ready(function()
 {   
  $('.close').click(function()   //back to last page
@@ -9,4 +10,26 @@ $(document).ready(function()
        
         window.location.assign('../letter/letter1.html')
      });
+   $("#pictureupload").change(function(){   //for upload picture
+      $('#form2').html('<h1 id="l">Loading...</h1>')
+        readURL(this); 
+
+   });
+ function readURL(input){    //to translate the upload picture into fancybox and store it into array
+   var img=new Array(input.files.length);  
+   for(var i = 0; i < input.files.length; i ++){
+     var reader = new FileReader();
+      reader.onload = function (e) {
+        img[i]=$("<img class='gallery' width='100%' height='100%'>").attr('src', e.target.result);
+        $('#form2').append(img[i]);
+        if(i==input.files.length)
+        $('#l').hide(); 
+      }
+      reader.readAsDataURL(input.files[i]);
+      
+    }
+  }   //end readurl
+
+
+  
 });
