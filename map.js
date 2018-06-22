82bba4958c460c6d5ba7cb1e7243bf199bb3fe27
Po-Chun, Yang <32588259+a29831968@users.noreply.github.com>
2018-06-23 00:43:04 +0800

    Update map.js

--- map.js
+++ map.js
@@ -1,3 +1,8 @@
 function initMap(){
-  console.log("in the function");
+  console.log("initMap");
+  // initialize the map
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: {lat: 22.999728, lng: 120.227028}
+  });
 }
